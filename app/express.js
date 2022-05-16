b64545f8a6d06720f40e7e9ea13b06300c6b03a9
Perman Yagmyrov <perman.yagmyrov@eloop.app>
2022-05-16 14:15:13 +0300

    vupd

--- app/express.js
+++ app/express.js
@@ -3,7 +3,7 @@ const express = require("express");
 const port = process.env.PORT || 3001;
 let app = express();
 app.get("/", (req, res) => {
-  res.send("oh hi ¯\_(ツ)_/¯ v1.0.4");
+  res.send("oh hi ¯\_(ツ)_/¯ v1.0.5");
 });
 app.use(express.json());
 app.listen(port);
