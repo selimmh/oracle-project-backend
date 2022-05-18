d5c345a9d04dc5a804ea0e24f43c36c53100f133
Perman Yagmyrov <perman.yagmyrov@eloop.app>
2022-05-18 11:19:55 +0300

    cors

--- app/express.js
+++ app/express.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const cors = require('cors')
 const port = process.env.PORT || 3001;
 let app = express();
 app.get("/", (req, res) => {
-  res.send("oh hi ¯\_(ツ)_/¯ v1.0.6");
+  res.send("oh hi ¯\_(ツ)_/¯ v1.0.7");
 });
 app.use(express.json());
+app.use(cors())
 app.listen(port);
 
 module.exports = app;
