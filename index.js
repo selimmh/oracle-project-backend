eb9a6aa60d2cbe7904d889939dd906fa266e7680
Perman Yagmyrov <perman.yagmyrov@eloop.app>
2022-05-29 17:24:16 +0300

    tratament

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = require("./app/express.js");
 const mediciRouter = require("./routes/mediciRouter");
 const programariRouter = require("./routes/programariRouter");
 const diagnosticRouter = require("./routes/diagnosticRouter");
+const tratamentRouter = require("./routes/tratamentRouter");
 const patientRouter = require("./routes/patientiRouter");
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
@@ -13,6 +14,7 @@ async function run() {
   app.use("/pacient", patientRouter);
   app.use("/programare", programariRouter);
   app.use("/diagnostic", diagnosticRouter);
+  app.use("/tratament", tratamentRouter);
 }
 
 run();
