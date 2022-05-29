f63a0e1747236446489d4610dc352cc837fa03f9
Perman Yagmyrov <perman.yagmyrov@eloop.app>
2022-05-29 16:52:57 +0300

    upd

---
+++ index.js
@@ -0,0 +1,16 @@
+const oracledb = require("oracledb");
+process.env.ORA_SDTZ = "UTC";
+const app = require("./app/express.js");
+const mediciRouter = require("./routes/mediciRouter");
+const programariRouter = require("./routes/programariRouter");
+const patientRouter = require("./routes/patientiRouter");
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+async function run() {
+  console.log("hellooo");
+  app.use("/medici", mediciRouter);
+  app.use("/pacient", patientRouter);
+  app.use("/programari", programariRouter);
+}
+
+run();
