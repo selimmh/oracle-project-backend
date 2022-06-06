c975f89710b08cd7db9a2f23fab2626efe3cc11d
Perman Yagmyrov <perman.yagmyrov@eloop.app>
2022-06-06 22:38:19 +0300

    upd

--- controllers/programariController.js
+++ controllers/programariController.js
@@ -10,42 +10,48 @@ async function GetProgramari(connection) {
   let programare;
   try {
     programare = await connection.execute(sql, binds, options);
-    sql2 = `SELECT * FROM pacient where id_pacient = :id`;
-    binds2 = {id:programare.rows[0].ID_PACIENT};
-    options2 = {
-      outFormat: oracledb.OUT_FORMAT_OBJECT,
-    };
-    let pacient;
-    try{
-      pacient = await connection.execute(sql2, binds2,options2);
-    }catch(e){
-      console.log(e);
-      return e;
+
+    let returnArray = [];
+    for (let i = 0; i < programare.rows.length; i++) {
+      sql2 = `SELECT * FROM pacient where id_pacient = :id`;
+      binds2 = { id: programare.rows[i].ID_PACIENT };
+      options2 = {
+        outFormat: oracledb.OUT_FORMAT_OBJECT,
+      };
+      let pacient;
+      try {
+        pacient = await connection.execute(sql2, binds2, options2);
+      } catch (e) {
+        console.log(e);
+        return e;
+      }
+      sql3 = `SELECT * FROM doctor where id_doctor = :id`;
+      binds3 = { id: programare.rows[i].ID_DOCTOR };
+      options3 = {
+        outFormat: oracledb.OUT_FORMAT_OBJECT,
+      };
+      let doctor;
+      try {
+        doctor = await connection.execute(sql3, binds3, options3);
+      } catch (e) {
+        console.log(e);
+        return e;
+      }
+      let returnData = {
+        ID_PACIENT: programare.rows[i].ID_PACIENT,
+        ID_DOCTOR: programare.rows[i].ID_DOCTOR,
+        ID_PROGRAMARE: programare.rows[i].ID_PROGRAMARE,
+        TRATAMENT: programare.rows[i].TRATAMENT,
+        MEDICAMENT: programare.rows[i].MEDICAMENT,
+        DATA_PROGRAMARE: programare.rows[i].DATA_PROGRAMARE,
+        pacient: pacient.rows[0],
+        doctor: doctor.rows[0],
+      };
+      returnArray.push(returnData);
     }
-    sql3 = `SELECT * FROM doctor where id_doctor = :id`;
-    binds3 = {id:programare.rows[0].ID_DOCTOR};
-    options3 = {
-      outFormat: oracledb.OUT_FORMAT_OBJECT,
-    };
-    let doctor;
-    try{
-      doctor = await connection.execute(sql3, binds3,options3);
-    }catch(e){
-      console.log(e);
-      return e;
-    }
-    
-    let returnData = {
-      ID_PACIENT:programare.rows[0].ID_PACIENT,
-      ID_DOCTOR:programare.rows[0].ID_DOCTOR,
-      ID_PROGRAMARE:programare.rows[0].ID_PROGRAMARE,
-      TRATAMENT:programare.rows[0].TRATAMENT,
-      MEDICAMENT:programare.rows[0].MEDICAMENT,
-      DATA_PROGRAMARE:programare.rows[0].DATA_PROGRAMARE,
-      pacient: pacient.rows[0],
-      doctor: doctor.rows[0],
-    }
-    return returnData;
+
+
+    return returnArray;
   } catch (e) {
     console.log(e);
     return e;
@@ -64,7 +70,6 @@ async function AddProgramari(connection, data) {
         4: data.idDoctor,
         5: data.tratament,
         6: data.medicament,
-      
       },
       { autoCommit: true }
     );
@@ -92,7 +97,7 @@ async function UpdateDataProgramari(connection, data) {
   try {
     result = await connection.execute(
       "UPDATE programare set DATA_PROGRAMARE = TO_TIMESTAMP(:time, 'YYYY-MM-DD HH24:MI') WHERE id_programare=:id ",
-      { time: data.time , id: data.programariId },
+      { time: data.time, id: data.programariId },
       { autoCommit: true }
     );
     console.log("Rows updated: " + result.rowsAffected);
